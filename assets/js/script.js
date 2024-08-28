d77d88861be9f83c739f3ea704dc8c62823da110
isadocirco <isabellacstcruz@gmail.com>
2024-08-28 18:34:38 +0000

    280824

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,27 @@
-let menuOpener = document.querySelector('.menu-opener');
-let nav = document.querySelector('header nav');
-
-menuOpener.addEventListener('click', () =>{
-    if(nav.classList.contains('opened')){
-        nav.classList.remove('opened');
-        menuOpener.querySelector('.close-icon').style .display = 'none';
-        menuOpener.querySelector('hamburger-icon').style .display ='flex';
-    }else{
-        nav.classList.add('opened');
-        menuOpener.querySelector('.close-icon').style .display ='flex';
-        menuOpener.querySelector('.hamburger-icon').style .display ='none';
-    }
+document.querySelector('.menu-opener')
+ .addEventListener('click', ()=>{
+   let nav=document.querySelector('header nav');
+   nav.classList.toggle('opened');
 });
+
+const toggleSlider = () => {
+  const slides = document.querySelectorAll('input[name=slider]');
+  if (slides.length === 0) return;
+
+  let current = document.querySelector('input[name=slider]:checked');
+  if (!current) {
+      current = document.querySelector('input[name=slider]');
+      current.setAttribute('checked', true);
+  }
+
+  let id = parseInt(current.getAttribute('id').split('-')[1]);
+  if (id + 1 <= slides.length) {
+      id++;
+  } else {
+      id = 1;
+  }
+
+  for (let slider of slides) slider.removeAttribute('checked');
+  document.querySelector(`#slider-${id}`).setAttribute('checked', true);
+}
+setInterval(toggleSlider, 5000);
